6830ca4316e18909c3f8e743db1275f9caeee36a
JinchengHeRyan <delteforce@outlook.com>
2022-03-18 18:18:09 +0800

    Add simple dataloader

---
+++ dataloader/__init__.py
@@ -0,0 +1,1 @@
+from .simple_dataloader import SimpleDataLoader
---
+++ dataloader/simple_dataloader.py
@@ -0,0 +1,25 @@
+from torch.utils.data import DataLoader
+import torch
+
+
+class SimpleDataLoader(DataLoader):
+    def __init__(
+        self, dataset, shuffle=False, batch_size=1, num_workers=1, drop_last=False
+    ):
+        self.dataset = dataset
+        super().__init__(
+            self.dataset,
+            batch_size=batch_size,
+            collate_fn=self.collate_fn,
+            num_workers=num_workers,
+            shuffle=shuffle,
+            drop_last=drop_last,
+            pin_memory=True,
+        )
+
+    def collate_fn(self, batch):
+        batch = list(zip(*batch))
+        (wav_utt, fbank, wav_spec, wav_phase, wav) = batch
+        wav = torch.stack(wav, dim=0)
+        wav_spec = torch.stack(wav_spec, dim=0)
+        return wav_utt, fbank, wav_spec, wav_phase, wav
