ae0d79aaf6d9bad535efba33f291edf18f68df5f
JinchengHeRyan <delteforce@outlook.com>
2022-03-20 00:54:11 +0800

    Add softmax into gvector

--- model/gvector.py
+++ model/gvector.py
@@ -5,12 +5,18 @@ from .resnet import ResNet, BasicBlock, Bottleneck
 
 
 class Gvector(nn.Module):
-    def __init__(self, channels, num_blocks, embd_dim, drop, class_num):
+    def __init__(
+        self, channels, num_blocks, embd_dim, drop, class_num, need_softmax=False
+    ):
         super(Gvector, self).__init__()
         self.resnet = ResNet(channels, BasicBlock, num_blocks)
         self.fc = nn.Linear(channels * 8 * 2, embd_dim)
         self.dropout = nn.Dropout(drop)
         self.linear = nn.Linear(embd_dim, class_num)
+        if need_softmax:
+            self.softmax = nn.Softmax()
+        else:
+            self.softmax = None
 
     def forward(self, x):
         x = self.resnet(x)
@@ -21,4 +27,6 @@ class Gvector(nn.Module):
         x = self.fc(x)
         x = self.dropout(x)
         x = self.linear(x)
+        if self.softmax is not None:
+            x = self.softmax(x)
         return x
